6d3f49fa160c2ddd1642df8a2701b9fd02720ace
Ravi Tambade <ravi.tambade@transflower.in>
2024-09-12 19:30:03 +0530

    standard c project build process

---
+++ Solutions/Std_CProject/include/math.h
@@ -0,0 +1,5 @@
+//function declaration
+int addition(int num1, int num2);
+int subtraction(int num1, int num2);
+int division(int num1, int num2);
+int multiplication(int num1, int num2);
---
+++ Solutions/Std_CProject/src/math.c
@@ -0,0 +1,21 @@
+#include "Math.h"
+//function implementation
+int addition(int num1, int num2) {
+    int result = num1 + num2;
+    return result;
+}
+
+int subtraction(int num1, int num2) {
+    int result = num1 - num2;
+    return result;
+}
+
+int multiplication(int num1, int num2) {
+    int result = num1 * num2;
+    return result;
+}
+
+int division(int num1, int num2) {
+    int result = num1 / num2;
+    return result;
+}
