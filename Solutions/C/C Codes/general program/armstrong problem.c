3b51caf007c3f61442917d51c737d0d171c820f5
Ravi Tambade <ravi.tambade@transflower.in>
2025-02-17 17:19:00 +0530

    changes

--- Solutions/C/C Codes/general program/armstrong problem.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int n,
-    printf ("enter number");
-    scanf("%d",&n);
-    while (n>0){
-    r=n%10;
-    }
-}
--- Solutions/C/C Codes/general program/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    int stroke ;
-    stroke  = 2 ;
-    int *ptrstroke=&stroke ;
-    *ptrstroke=4; 
-    printf(" the value of the stroke is:%d\n",*ptrstroke );
-    printf(" the value of the stroke is:%d\n",ptrstroke );
-}
