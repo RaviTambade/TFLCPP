51148532cc456f1e359e16b7407c32634c721d6d
Ravi Tambade <ravi.tambade@transflower.in>
2025-02-18 11:55:03 +0530

    union

---
+++ Solutions/DSA/Unions/shape.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+union Shape{
+    struct  {float radius;} circle;
+    struct  {float length, width} reactangle;
+    struct  {float base; height } triangle;
+};
+
+union SensorData{
+    float temperature;
+    float presssure;
+    float humidity;
+    float luminocity;
+    float waterMoistureLevel;
+    float waterLevelIntax;
+    float waterInTank;
+};
+
+union Vehicle{
+    struct { int wheels; int doors} car;
+    struct {int wheels} bike;
+    struct {int wheel; int capacity} bus;
+};
+
+
+int main(){
+
+    int count=34;
+    union Shape geometricShape;
+    geometricShape.circle.radius=25;
+    geometricShape.reactangle.length=45;
+    geometricShape.reactangle.width=75;
+    geometricShape.triangle.base=23;
+    geometricShape.triangle.height=15;
+
+    printf( "Circle Radius= % .2f\n", geometricShape.circle.radius);
+    printf( "Rectangle lengith= % .2f,  width=% .2f\n",geometricShape.reactangle.length,geometricShape.reactangle.width);
+    printf( "triangle base= % .2f,  height=% .2f\n",geometricShape.triangle.base,geometricShape.triangle.height);
+
+}
